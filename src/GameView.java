6dc1a3e306717c75bde22fa0b25d3c1b8367ba8b
RickDalexis <Riccardi.Dalexis@gmail.com>
2017-10-05 10:09:42 -0400

    Added a rectangle  :)
    Figured out how to insert a rectangle. I'm going to add a fun intro sequence next.

--- src/GameView.java
+++ src/GameView.java
@@ -5,6 +5,8 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 
 import java.io.IOException;
 
@@ -18,6 +20,14 @@ public class GameView {
         screen.startScreen();
         // you must clear the screen in a loop to display the  graphics
         screen.clear();
+
+        // this is all to draw a rectangle 
+        screen.newTextGraphics().drawRectangle(
+                new TerminalPosition(3,3), new TerminalSize(10,10), '*');
+        screen.refresh();
+
+        screen.readInput();
+        screen.stopScreen();
 
         {
         }
